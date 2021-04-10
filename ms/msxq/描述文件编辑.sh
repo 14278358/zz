07aa9f9ada76e63a6408d66725511bcf3192236d
14278358 <81848727+14278358@users.noreply.github.com>
2021-04-10 03:46:09 -0700

    2

--- ms/msxq/描述文件编辑.sh
+++ ms/msxq/描述文件编辑.sh
@@ -5,7 +5,7 @@ perl -p -i -e "s/插件的描述文字/对设置里面的标题分类./g"  cydia
 perl -p -i -e "s/IOS:11-12/IOS:11-12/g"  cydia副本 sileo副本.json
 perl -p -i -e "s/1.0.0/0.9.6b/g"  cydia副本 sileo副本.json
 #更新时间
-perl -p -i -e "s/2019-01-21/2019-05-04/g"  sileo副本.json
+perl -p -i -e "s/2021-04-10/2021-04-10/g"  sileo副本.json
 perl -p -i -e "s/暂时没有历史版本/2019-05-04更新支持A12/g"  cydia副本
 #截图
 #perl -p -i -e "s/1.png/slices21.png/g"   sileo副本.json
